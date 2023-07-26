41a0245b60804fcf7ed1354da9d6adbfbae687c6
df@ <@>
2023-07-26 23:27:02 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::io::Cursor;
 use image::io::Reader as ImageReader;
+use image::{RgbImage, Rgba};
 
 const IMAGE: &str = "test_images/black_bg_linear_rgb_32bit_float.exr";
 const BURNIN: &str = "test_images/burnin0.exr";
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let img = ImageReader::open(IMAGE)?.decode()?;
-    let burnin = ImageReader::open(BURNIN)?.decode()?;
+    let mut img = ImageReader::open(IMAGE)?.decode()?.to_rgba32f();
+
+    for x in 0..800 {
+        for y in 0..400 {
+            img.put_pixel(x, y, Rgba([0 as f32, (x % 2) as f32, y as f32, 0.0]))
+        }
+    }
     Ok(())
 }
