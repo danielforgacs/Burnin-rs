2ed3eeebc512cfdf69b9c7ac539e0bf721fcaca7
df@ <@>
2023-07-26 23:00:59 +0100

    opening images.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Cursor;
+use image::io::Reader as ImageReader;
+
+const IMAGE: &str = "test_images/black_bg_linear_rgb_32bit_float.exr";
+const BURNIN: &str = "test_images/burnin0.exr";
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let img = ImageReader::open(IMAGE)?.decode()?;
+    let burnin = ImageReader::open(BURNIN)?.decode()?;
+    Ok(())
 }
