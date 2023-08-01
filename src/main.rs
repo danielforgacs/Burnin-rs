61f125f4bbc37925e355257789972abc49f5675e
df@ <@>
2023-08-01 23:47:55 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,19 @@ const BURNIN: &str = "render/burnin_uv_map_1280x720_32f_rgba_no_premult.exr";
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut img = ImageReader::open(IMAGE)?.decode()?.to_rgba32f();
-    let burnin = ImageReader::open(BURNIN)?.decode()?.to_rgb32f();
+    let burnin = ImageReader::open(BURNIN)?.decode()?.to_rgba32f();
     let mut img_new = Rgb32FImage::new(1500, 1500);
 
 
     for y in 0..255 {
         for x in 0..255 {
-            let bin = burnin.get_pixel(x, y);
             let col = img.get_pixel(x, y);
-            let new_col = [col[0] * col[3], col[1] * col[3], col[2] * col[3]];
+            let bin = burnin.get_pixel(x, y);
+            let new_col = [
+                bin[0] * (1.0 - bin[3]),
+                bin[1] * (1.0 - bin[3]),
+                bin[2] * (1.0 - bin[3])
+            ];
             img_new.put_pixel(x, y, Rgb([new_col[0], new_col[1], new_col[2]]));
         }
     }
