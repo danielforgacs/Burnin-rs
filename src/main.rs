1b0ed426c64c2d975555661822759940cf80f52e
df@ <@>
2023-08-06 23:54:40 +0100

    simple over comp works.

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,26 @@ use image::{RgbImage, Rgb, Rgba, Rgba32FImage, Rgb32FImage};
 
 
 
-const IMAGE: &str = "rnd_images/AllHalfValues.exr";
-const BURNIN: &str = "render/burnin_uv_map_1280x720_32f_rgba_no_premult.exr";
+// const IMAGE: &str = "rnd_images/AllHalfValues.exr";
+// const BURNIN: &str = "render/burnin_uv_map_1280x720_32f_rgba_no_premult.exr";
+const IMAGE: &str = "rnd_images/Desk.exr";
+const BURNIN: &str = "render/burnin_solidcolour_644x874_32f_rgba_no_premult.exr";
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let mut img = ImageReader::open(IMAGE)?.decode()?.to_rgba32f();
-    dbg!(&img.width());
-    dbg!(&img.height());
+    let img = ImageReader::open(IMAGE)?.decode()?.to_rgba32f();
+    let (width, height) = img.dimensions();
     let burnin = ImageReader::open(BURNIN)?.decode()?.to_rgba32f();
-    dbg!(&burnin.width());
-    dbg!(&burnin.height());
-    let mut img_new = Rgb32FImage::new(1500, 1500);
+    let mut img_new = Rgb32FImage::new(width, height);
 
 
-    for y in 0..255 {
-        for x in 0..255 {
+    for y in 0..height {
+        for x in 0..width {
             let col = img.get_pixel(x, y);
             let bin = burnin.get_pixel(x, y);
             let new_col = [
-                (bin[0] * (1.0 - bin[3])) + (col[0] * bin[3]),
-                (bin[1] * (1.0 - bin[3])) + (col[1] * bin[3]),
-                (bin[2] * (1.0 - bin[3])) + (col[2] * bin[3]),
+                (bin[0] * bin[3]) + (col[0] * (1.0 - bin[3])),
+                (bin[1] * bin[3]) + (col[1] * (1.0 - bin[3])),
+                (bin[2] * bin[3]) + (col[2] * (1.0 - bin[3])),
             ];
             img_new.put_pixel(x, y, Rgb([new_col[0], new_col[1], new_col[2]]));
         }
