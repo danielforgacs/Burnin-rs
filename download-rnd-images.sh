ce4aead86078e18772a47ee27b356f42bd4d6a65
df@ <@>
2023-08-07 14:42:18 +0100

    updated test image downloader.

--- download-rnd-images.sh
+++ download-rnd-images.sh
@@ -2,11 +2,10 @@
 # https://openexr.com/en/latest/_test_images/index.html#test-images
 
 img_dir=rnd_images
-if [ -d $img_dir ]
+if [ ! -d $img_dir ]
 then
-    return
+    mkdir $img_dir
 fi
-mkdir $img_dir
 
 curl https://raw.githubusercontent.com/AcademySoftwareFoundation/openexr-images/main/TestImages/AllHalfValues.exr -o $img_dir/AllHalfValues.exr
 curl https://raw.githubusercontent.com/AcademySoftwareFoundation/openexr-images/main/ScanLines/Desk.exr -o $img_dir/Desk.exr
